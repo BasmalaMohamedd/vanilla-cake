b595d5cd985e89e70f6a5fd1d270b193c8c07f49
BasmalaMohamedd <basmalamohamed934@gmail.com>
2025-06-26 22:24:06 +0300

    ingrediant form

--- src/components/IngredientForm.jsx
+++ src/components/IngredientForm.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+
 export default function IngrediantForm(){
+    const [ingrediants, setIngrediants] = useState(["floor", "eggs", "milk", "oil", "sugar", "vanilla"]);
+    const ingrediantsElement = ingrediants.map((ingrediant)=><li key={ingrediant}>{ingrediant}</li>)
+    function addIngrediant(formData)
+    {
+        //react 19
+        const newIngrediant = formData.get("ingrediant");
+        setIngrediants(preIngrediants=>[...preIngrediants, newIngrediant]);
+        console.log(ingrediants);
+    }
     return (
-        <form className="add-ingrediant-form">
-            <input type="text" placeholder="e.g. cinnamon" aria-label="Add ingrediant"/>
-            <button>+ Add ingrediant</button>
-        </form>
+        <>
+            <form action={addIngrediant} className="add-ingrediant-form">
+                <input type="text" placeholder="e.g. cinnamon" aria-label="Add ingrediant" name="ingrediant"/>
+                <button type="submit">+ Add ingrediant</button>
+            </form>
+            <ul>
+                {ingrediantsElement}
+                
+            </ul>
+        </>
+        
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,11 +2,12 @@ import IngrediantForm from "./IngredientForm";
 
 
 export default function Main(){
-
+    const ingrediants = ["floor", "eggs", "milk", "oil", "sugar", "vanilla"]
+    const ingrediantsElement = ingrediants.map((ingrediant)=><li key={ingrediant}>{ingrediant}</li>)
     return(
         <main>
             <IngrediantForm />
-            This is the main
+            
         </main>
     )
 }
